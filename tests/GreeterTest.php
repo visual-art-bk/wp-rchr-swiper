6cf222e257e7571bfa561642fb584602895bedc2
kbk <kekekj@naver.com>
2024-10-02 13:03:53 +0900

    first commit

---
+++ tests/GreeterTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+include './Greeter.php';
+
+use PHPUnit\Framework\TestCase;
+
+
+final class GreeterTest extends TestCase
+{
+    public function testGreetsWithName(): void
+    {
+        $greeter = new Greeter;
+
+        $greeting = $greeter->greet('Alice');
+
+        $this->assertSame('Hello, Alice!', $greeting);
+    }
+}
---
+++ wp-rchr-swiper.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: wp-rchr-swiper
+ * Description: 뤼초록에서 개발한 스와이퍼입니다.
+ * Author: 뤼초록 김병관
+ * Version: 1.0.0
+ */
+function admin_wp_rchr_swiper() {
+
+}
+add_action('admin_menu', 'admin_wp_rchr_swiper');
+
+
